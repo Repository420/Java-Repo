d3a288fdf6d1cbd1283c65ec17f1107001c92461
Bastian Stefan Brandl <it23_bbra@bappassau.onmicrosoft.com>
2025-12-05 11:32:25 +0100

    Linked-List Logik Implementiert

---
+++ MyList/src/main/java/org/example/List.java
@@ -0,0 +1,16 @@
+package org.example;
+
+class Node{
+    int Data;
+    Node next;
+
+    Node(int new_data){
+        this.Data = new_data;
+        this.next = null;
+    }
+
+}
+
+public class List {
+
+}
