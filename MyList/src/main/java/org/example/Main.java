07cf881e184aa10009552f293008e8878e7edff1
Bastian Stefan Brandl <it23_bbra@bappassau.onmicrosoft.com>
2026-01-16 11:31:11 +0100

    Test

--- MyList/src/main/java/org/example/Main.java
+++ MyList/src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-
 public class Main {
     static void main() {
 
---
+++ TikTakTo/src/main/java/org/example/Player.java
@@ -0,0 +1,6 @@
+package org.example;
+
+interface Player {
+    void move(Board board);
+    char getSymbol();
+}
